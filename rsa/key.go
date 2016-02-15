a3b51fb76e98de23638e071acb0ff6d5ac9a8780
baisu <631531720@qq.com>
2016-02-15 16:39:18 +0800

    新增了对pkcs1 秘钥的支持

--- rsa/key.go
+++ rsa/key.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"encoding/base64"
 	"errors"
 	"strings"
 	"io/ioutil"
@@ -29,27 +28,17 @@ func ParsePKCS8Key(publicKey, privateKey []byte) (Key, error) {
 	return &key{publicKey:puk.(*rsa.PublicKey), privateKey:prk.(*rsa.PrivateKey)}, nil
 }
 
-func ParsePKCS8KeyWithBase64(publicKey, privateKey string) (Key, error) {
-	puk, err := base64.StdEncoding.DecodeString(publicKey)
+func ParsePKCS1Key(publicKey, privateKey []byte) (Key, error) {
+	puk, err := x509.ParsePKIXPublicKey(publicKey)
 	if err != nil {
 		return nil, err
 	}
-	prk, err := base64.StdEncoding.DecodeString(privateKey)
+
+	prk, err := x509.ParsePKCS1PrivateKey(privateKey)
 	if err != nil {
 		return nil, err
 	}
-	return ParsePKCS8Key(puk, prk)
-}
-
-func ParsePKCS8KeyWithPEMEncoding(publicKey, privateKey string) (Key, error) {
-
-	puk, _ := pem.Decode([]byte(publicKey))
-	prk, _ := pem.Decode([]byte(privateKey))
-
-	if puk == nil || prk == nil {
-		return nil, errors.New("is not pem formate")
-	}
-	return ParsePKCS8Key(puk.Bytes, prk.Bytes)
+	return &key{publicKey:puk.(*rsa.PublicKey), privateKey:prk.(*rsa.PrivateKey)}, nil
 }
 
 func LoadPKCS8KeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string) (Key, error) {
@@ -81,6 +70,37 @@ func LoadPKCS8KeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string) (Key,
 	}
 
 	return ParsePKCS8Key(puk.Bytes, prk.Bytes)
+}
+
+func LoadPKCS1KeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string) (Key, error) {
+
+	//TODO 断言如果入参为"" ，则直接报错
+
+	publicKeyFilePath = strings.TrimSpace(publicKeyFilePath)
+
+	pukBytes, err := ioutil.ReadFile(publicKeyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	puk, _ := pem.Decode(pukBytes)
+	if puk == nil {
+		return nil, errors.New("publicKey is not pem formate")
+	}
+
+	privateKeyFilePath = strings.TrimSpace(privateKeyFilePath)
+
+	prkBytes, err := ioutil.ReadFile(privateKeyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	prk, _ := pem.Decode(prkBytes)
+	if prk == nil {
+		return nil, errors.New("privateKey is not pem formate")
+	}
+
+	return ParsePKCS1Key(puk.Bytes, prk.Bytes)
 }
 
 type key struct {
