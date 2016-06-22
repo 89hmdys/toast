06e4498964542af57f4c83d47c7379af6e6d13d1
baisu <631531720@qq.com>
2016-06-22 21:53:31 +0800

    新增了oaep模式

--- rsa/mode.go
+++ rsa/mode.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"errors"
+	"hash"
 )
 
 type CipherMode interface {
@@ -19,12 +20,6 @@ type pkcs1v15Cipher cipherMode
 func NewPKCS1v15Cipher() CipherMode {
 	return new(pkcs1v15Cipher)
 }
-
-//type oaepCipher  cipherMode
-//
-//func NewOAEPCipher() CipherMode {
-//	return new(oaepCipher)
-//}
 
 func (pkcs1v15 *pkcs1v15Cipher) Encrypt(plainText []byte, puk *rsa.PublicKey) ([]byte, error) {
 	return rsa.EncryptPKCS1v15(rand.Reader, puk, plainText)
@@ -70,4 +65,18 @@ func (pkcs1v15 *pkcs1v15Sign) Verify(src []byte, sign []byte, hash crypto.Hash, 
 	return rsa.VerifyPKCS1v15(puk, hash, hashed, sign)
 }
 
-//type pss  signMode
+//type oaepCipher struct {
+//	h hash.Hash
+//}
+//
+//func NewOAEPCipher() CipherMode {
+//	return new(oaepCipher)
+//}
+//
+//func (oaep *oaepCipher) Encrypt(plainText []byte, puk *rsa.PublicKey) ([]byte, error) {
+//	return rsa.EncryptOAEP(oaep.h, rand.Reader, puk, plainText, make([]byte, 0))
+//}
+//
+//func (oaep *oaepCipher) Decrypt(cipherText []byte, prk *rsa.PrivateKey) ([]byte, error) {
+//	return rsa.DecryptOAEP(oaep.h, rand.Reader, prk, cipherText, make([]byte, 0))
+//}
