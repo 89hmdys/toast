5ad800b96018e513c492f6de50964f5df914c313
QuanTran <quan.tranhoang@mmoherovietnam.com>
2022-07-22 16:37:48 +0700

    Add LoadKeyFromPEMByte and ParsePKCS1KeyByCert from Eason Lin

--- rsa/key.go
+++ rsa/key.go
@@ -40,6 +40,18 @@ func ParsePKCS1Key(publicKey, privateKey []byte) (Key, error) {
 	return &key{publicKey: puk.(*rsa.PublicKey), privateKey: prk}, nil
 }
 
+func ParsePKCS1KeyByCert(publicKey, privateKey []byte) (Key, error) {
+	puk, err := x509.ParseCertificate(publicKey)
+	if err != nil {
+		return nil, err
+	}
+	prk, err := x509.ParsePKCS1PrivateKey(privateKey)
+	if err != nil {
+		return nil, err
+	}
+	return &key{publicKey: puk.PublicKey.(*rsa.PublicKey), privateKey: prk}, nil
+}
+
 func LoadKeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string, ParseKey func([]byte, []byte) (Key, error)) (Key, error) {
 
 	//TODO 断言如果入参为"" ，则直接报错
@@ -69,6 +81,40 @@ func LoadKeyFromPEMFile(publicKeyFilePath, privateKeyFilePath string, ParseKey f
 	}
 
 	return ParseKey(puk.Bytes, prk.Bytes)
+}
+
+//Eason Lin
+func LoadKeyFromPEMByte(pukBytes, prkBytes []byte, ParseKey func([]byte, []byte) (Key, error)) (Key, error) {
+	puk, _ := pem.Decode(pukBytes)
+	if puk == nil {
+		return nil, errors.New("publicKey is not pem formate")
+	}
+
+	prk, _ := pem.Decode(prkBytes)
+	if prk == nil {
+		return nil, errors.New("privateKey is not pem formate")
+	}
+
+	return ParseKey(puk.Bytes, prk.Bytes)
+}
+
+func LoadKeyFromDerFile(publicKeyFilePath, privateKeyFilePath string, ParseKey func([]byte, []byte) (Key, error)) (Key, error) {
+
+	publicKeyFilePath = strings.TrimSpace(publicKeyFilePath)
+
+	pukBytes, err := ioutil.ReadFile(publicKeyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	privateKeyFilePath = strings.TrimSpace(privateKeyFilePath)
+
+	prkBytes, err := ioutil.ReadFile(privateKeyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseKey(pukBytes, prkBytes)
 }
 
 type key struct {
